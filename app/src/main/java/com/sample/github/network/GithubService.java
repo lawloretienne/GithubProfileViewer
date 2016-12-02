d09093d935b3c4330ef0f9c957fd56763c728134
Etienne Lawlor <lawloretienne@gmail.com>
2016-12-02 09:44:19 -0800

    Bug fixes

--- app/src/main/java/com/sample/github/network/GithubService.java
+++ app/src/main/java/com/sample/github/network/GithubService.java
@@ -19,12 +19,12 @@ public interface GithubService {
 
     String BASE_URL = "https://api.github.com/users/";
 
-    @GET("{username}?client_id=378faee748383b4d5290&client_secret=0f5e45f7526789f077e9b0d2e052718b5122aecc")
+    @GET("{username}")
     Call<User> getUser(@Path("username") String username);
 
-    @GET("{username}/repos?sort=pushed&direction=desc&client_id=378faee748383b4d5290&client_secret=0f5e45f7526789f077e9b0d2e052718b5122aecc")
+    @GET("{username}/repos?sort=pushed&direction=desc")
     Call<List<Repository>> getRepositories(@Path("username") String username, @Query("page") int page, @Query("per_page") int per_page);
 
-    @GET("{username}/events?client_id=378faee748383b4d5290&client_secret=0f5e45f7526789f077e9b0d2e052718b5122aecc")
+    @GET("{username}/events")
     Call<List<Event>> getEvents(@Path("username") String username, @Query("page") int page, @Query("per_page") int per_page);
 }
